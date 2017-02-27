f58eb07cc0f25c2235a10b7bfeea75b6387d597a
nel215 <otomo.yuhei@gmail.com>
2017-02-27 21:59:51 +0900

    Add a method to display history

---
+++ history.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/nel215/atcmd/login"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func DescribeHistory(sess *login.Session) error {
+	req, err := http.NewRequest(http.MethodGet, "https://practice.contest.atcoder.jp/submissions/me", nil)
+	if err != nil {
+		return err
+	}
+	sess.AddSessionCookies(req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	log.Println(string(body))
+	return nil
+}
