131f92a7d7d7b22342e9da425eeb7ca3a65c35ea
nel215 <otomo.yuhei@gmail.com>
2017-02-27 20:06:20 +0900

    Add a method to describe the id and name of the problems

---
+++ problem.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nel215/atcmd/login"
+	"golang.org/x/net/html"
+	"net/http"
+)
+
+func DescribeProblem(sess *login.Session) error {
+	req, err := http.NewRequest(http.MethodGet, "https://practice.contest.atcoder.jp/submit", nil)
+	sess.AddSessionCookies(req)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	z := html.NewTokenizer(resp.Body)
+
+	InTaskSelector := false
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			return nil
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			if t.Data == "select" {
+				InTaskSelector = checkTaskSelector(t)
+				if InTaskSelector {
+					fmt.Printf("# Problems\n\n")
+				}
+			}
+
+			if !InTaskSelector {
+				continue
+			}
+
+			if t.Data == "option" {
+				id := extractTaskId(t)
+				z.Next()
+				t = z.Token()
+				fmt.Printf("- %s: %s\n", id, t.Data)
+			}
+		case tt == html.EndTagToken:
+			t := z.Token()
+
+			if t.Data == "select" {
+				InTaskSelector = false
+			}
+		}
+	}
+}
+
+func checkTaskSelector(t html.Token) bool {
+	for _, a := range t.Attr {
+		if a.Key == "name" && a.Val == "task_id" {
+			return true
+		}
+	}
+	return false
+}
+
+func extractTaskId(t html.Token) string {
+	for _, a := range t.Attr {
+		if a.Key == "value" {
+			return a.Val
+		}
+	}
+	return ""
+}
