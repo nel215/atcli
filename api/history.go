c8731e3140a1f62703b2cb5961517adcaea0339a
nel215 <otomo.yuhei@gmail.com>
2017-03-01 00:09:13 +0900

    Fix history format

--- api/history.go
+++ api/history.go
@@ -1,10 +1,11 @@
 package api
 
 import (
+	"fmt"
 	"github.com/nel215/atcli/session"
-	"io/ioutil"
-	"log"
+	"golang.org/x/net/html"
 	"net/http"
+	"strings"
 )
 
 func DescribeHistory(sess *session.Session) error {
@@ -18,10 +19,44 @@ func DescribeHistory(sess *session.Session) error {
 		return err
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
+
+	z := html.NewTokenizer(resp.Body)
+	texts := make([]string, 0)
+	fmt.Print("# History\n\n")
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			return nil
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			if t.Data != "td" {
+				continue
+			}
+			for {
+				z.Next()
+				t = z.Token()
+				if t.Data == "td" {
+					break
+				}
+				if t.Type != html.TextToken {
+					continue
+				}
+				texts = append(texts, t.Data)
+			}
+
+		case tt == html.EndTagToken:
+			t := z.Token()
+
+			if t.Data == "tr" {
+				if len(texts) > 0 {
+					fmt.Printf("- %s\n", strings.Join(texts, ", "))
+				}
+			}
+		}
 	}
-	log.Println(string(body))
+
 	return nil
 }
